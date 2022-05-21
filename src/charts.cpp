da5c21a670590237acce94a7c6a0ba60a90039c7
Matthew Early <matthewjearly@gmail.com>
2022-05-20 21:00:58 -0400

    use runtime dir for pathing

--- src/charts.cpp
+++ src/charts.cpp
@@ -74,10 +74,12 @@ void Charts::displayExperienceChart()
   pressEnterToContinue();
 }
 
+extern std::string RUNTIME_PATH;
+
 void Charts::displayPoisonSalesChart()
 {
   fstream poisonfile;
-  poisonfile.open(DATA_DIR + "poisonlist.dat");
+  poisonfile.open(RUNTIME_PATH + DATA_DIR + "poisonlist.dat");
   if (!poisonfile.is_open())
   {
    cout << "Error Opening poisonlist.dat, check your file.\n\n";
@@ -98,7 +100,7 @@ void Charts::displayPoisonSalesChart()
 void Charts::displayDiseaseChart()
 {
   fstream diseasefile;
-  diseasefile.open(DATA_DIR + "diseaselist.dat");
+  diseasefile.open(RUNTIME_PATH + DATA_DIR + "diseaselist.dat");
   if (!diseasefile.is_open())
   {
     cout << "Error Opening diseaselist.dat, check your file.\n\n";
@@ -122,7 +124,7 @@ void Charts::displayDiseaseChart()
 void Charts::displayMadnessChart()
 {
   fstream madnessfile;
-  madnessfile.open(DATA_DIR + "madnesslist.dat");
+  madnessfile.open(RUNTIME_PATH + DATA_DIR + "madnesslist.dat");
   if (!madnessfile.is_open())
   {
     cout << "Error Opening madnesslist.dat, check your file.\n\n";
